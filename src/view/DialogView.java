cd07a94cb55206d3b8c38cc6513e03f0ff213b92
XiaoMao <XiaoMao@DESKTOP-ISTUQ5V>
2020-07-04 10:04:13 +0800

    2

--- src/view/DialogView.java
+++ src/view/DialogView.java
@@ -97,15 +97,7 @@ class InputDialog extends JDialog implements ActionListener
 		{
 			this.setVisible(false);
 		}    
-    }
-	
-//	public void FileIn(String ad, String s) throws IOException// 写入文件
-//	{	
-//
-//		FileWriter writer = new FileWriter(ad, true);
-//        writer.write("\n" + s);
-//        writer.close();
-//	}	
+    }	
 }
 
 
@@ -185,20 +177,6 @@ class TopDialog extends JDialog implements ActionListener
     {
         this.setVisible(false);       
     }
-    
-//    public void FileOut(String ad) throws IOException// 读取文件
-//	{
-//		String thisLine;
-//		BufferedReader in = new BufferedReader(new FileReader(ad)); // 创建缓存区字符输入流，需要传如Reader对象
-//		while((thisLine = in.readLine()) != null)    // 每次读取一行，直到文件结束
-//		{
-//			String name = thisLine;
-//			int time = Integer.parseInt(in.readLine());
-//			Record q = new Record(name, time);
-//			L.SortIn(q);						
-//		}
-//		in.close();
-//	}
 }
 
 
